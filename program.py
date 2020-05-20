c4a8a335234c017c65c44e1192769ff2282b5c17
wilfhawk <33912780+wilfhawk@users.noreply.github.com>
2020-05-19 21:26:39 -0400

    Initial commit

---
+++ program.py
@@ -0,0 +1,8 @@
+print('---------------------')
+print('   HELLO APP')
+print('---------------------')
+print()
+# TODO I need to add to this
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you '
+print(greeting + user_text)
